4e861345f7d748b21a95f1122b39cedb4ad6fdf7
Adarsh <tenminusthreeseven@gmail.com>
2026-02-10 12:07:57 +0530

    Add solution for Longest Balanced Subarray I - LeetCode

---
+++ Longest_Balanced_Subarray_I_LeetCode/solution.js
@@ -0,0 +1,12 @@
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+int ans = 0;
+for(int i=0;i<n;i++){
+    unordered_set<int> even;
+    unordered_set<int> odd;
+    for(int j =i;j<n;j++){
+        if(nums[j]%2==0){
+            even.insert(nums[j]);
+        } else {
+            odd.insert(nums[j]);
+        }
