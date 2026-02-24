d667bf9624e49e8566d6ec4ba32f3790292cf999
Adarsh <tenminusthreeseven@gmail.com>
2026-02-24 17:13:17 +0530

    Add solution for Sum of Root To Leaf Binary Numbers - LeetCode

---
+++ Sum_of_Root_To_Leaf_Binary_Numbers_LeetCode/solution.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+int dfs(TreeNode * root,int current){
+if(!root) return 0;
+
+
+current = current *2 +root->val;
+
+while(!root->left && !root->right){
+    return current;
+}
+return dfs(root->left, current) + dfs(root->right, current);
+
+
+}
+
+
+
+
+
+    int sumRootToLeaf(TreeNode* root) {
+       return  dfs(root,0);
+
+
