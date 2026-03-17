b2f3a77903cbb0cc64657ae3f4fc62b0278c2eeb
Adarsh <tenminusthreeseven@gmail.com>
2026-03-17 12:26:45 +0530

    Add solution for Min Cost Climbing Stairs - LeetCode

--- Min_Cost_Climbing_Stairs_LeetCode/solution.js
+++ Min_Cost_Climbing_Stairs_LeetCode/solution.js
@@ -1,10 +1,3 @@
-class Solution {
-public:
-
-int solve(vector<int>&cost , int n, vector<int> & dp){
-    if(n==0) return cost[0];
-    if(n==1) return cost[1];
-if(dp[n]!= -1){
     return dp[n]; //step - 3
 
 }
@@ -12,13 +5,25 @@ if(dp[n]!= -1){
     dp[n] = cost[n]+ min(solve(cost,n-1,dp), solve(cost,n-2,dp)); //step-2
     return dp[n];
 }
+
+int solve2(vector<int> &cost, int n){
+    vector<int> dp(n+1);
+    dp[0] =cost[0];
+    dp[1]= cost[1];
+
+    for(int i=2;i<n;i++){
+        dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+    }
+
+    return min(dp[n-1], dp[n-2]);
+}
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
 
       
 
-        vector<int> dp(n+1, -1); // step -1
-        int ans = min(solve(cost, n-1,dp),solve(cost,n-2,dp));
-        return ans;
-    }
+        // vector<int> dp(n+1, -1); // step -1
+        // int ans = min(solve(cost, n-1,dp),solve(cost,n-2,dp));
+        // return ans;
+        return solve2(cost,n);}
 };
