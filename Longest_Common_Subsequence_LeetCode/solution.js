e7c97778e71ec4a609c77c4ae0e676c8fd847651
Adarsh <tenminusthreeseven@gmail.com>
2026-04-20 20:41:14 +0530

    Add solution for Longest Common Subsequence - LeetCode

---
+++ Longest_Common_Subsequence_LeetCode/solution.js
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int x = text1.length();
+        int y = text2.length();
+
+        vector<vector<int>>dp(x+1,vector<int>(y+1,0));
+        for(int i=1;i<=x;i++){
+for(int j=1;j<=y;j++){
+
+    if(text1[i-1]== text2[j-1]){
+        dp[i][j] = 1 + dp[i-1][j-1];
+    } else {
+     dp[i][j] =max(dp[i-1][j], dp[i][j-1]);
+    }
+}
+        }
+        return dp[x][y];
+    }
+};
